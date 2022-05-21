7bd7d75fd4f671a424deff9ca0a11d8198cd28a1
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-05-22 08:08:06 +0900

    Initial Commit

---
+++ Money Management/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money Management
+//
+//  Created by 春田実利 on 2022/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Money ManagementUITests/Money_ManagementUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Money_ManagementUITestsLaunchTests.swift
+//  Money ManagementUITests
+//
+//  Created by 春田実利 on 2022/05/22.
+//
+
+import XCTest
+
+class Money_ManagementUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
