2fad68168b960bae7e43fbe49995e65f348c8dfa
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-05-22 11:08:53 +0900

    1回目

---
+++ Money Management/ManagementViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ManagementViewController.swift
+//  Money Management
+//
+//  Created by 春田実利 on 2022/05/22.
+//
+
+import UIKit
+
+class ManagementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
