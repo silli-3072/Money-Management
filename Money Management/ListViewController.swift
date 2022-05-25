4d9aaded2e5feef11ef19eb91d309bc2e10d8021
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-05-25 19:19:36 +0900

    5/25夜　（再）

--- Money Management/ListViewController.swift
+++ Money Management/ListViewController.swift
@@ -20,7 +20,6 @@ class ListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
