cfa86255fa25b0914ba73e6155c49304315e4188
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-05-26 10:56:21 +0900

    5/26昼

--- Money Management/RegisterViewController.swift
+++ Money Management/RegisterViewController.swift
@@ -11,6 +11,10 @@ import Eureka
 class RegisterViewController: FormViewController {
     
     let userDefaults = UserDefaults.standard
+    
+    var incomejudge: String = ""
+    
+    
     //let day = Date()
 
     override func viewDidLoad() {
@@ -20,85 +24,87 @@ class RegisterViewController: FormViewController {
         <<< SegmentedRow<String>("income"){ row in
             row.options = ["支出", "収入"]
             row.value = "支出"
-            
-            if row.value == "支出" {
-                form +++ Section()
-                   <<< DateInlineRow("date"){
-                        $0.title = "日付"
-                        //$0.value = "Date"
-                       
-                       UserDefaults.standard.data(forKey: "Date")
-                }
-                
-                form +++ Section()
-                  <<< PushRow<String>("method"){
-                      $0.title = "支払い方法"
-                      $0.options = ["現金","キャッシュレス決済","交通ICカード","その他"]
-                      $0.value = "現金"
-                      
-                      UserDefaults.standard.string(forKey: "PaymentMethod")
-                }
-                
-                form +++ Section()
-                    <<< IntRow("amountofmoney"){
-                        $0.title = "金額"
-                        
-                        UserDefaults.standard.string(forKey: "Value")
-                }
-                
-                form +++ Section()
-                    <<< PushRow<String>("classification"){
-                        $0.title = "分類"
-                        $0.options = ["食費","交通費","交際費","娯楽費","被服費","その他"]
-                        $0.value = "食費"
-                        
-                        UserDefaults.standard.string(forKey: "Group")
-                }
-            } else if row.value == "収入" {
-                form +++ Section()
-                   <<< DateInlineRow("date"){
-                        $0.title = "日付"
-                        //$0.value = "Date"
-                       
-                       UserDefaults.standard.data(forKey: "Date")
-                }
-                
-                form +++ Section()
-                    <<< PushRow<String>("register"){
-                        $0.title = "登録先"
-                        $0.options = ["現金","キャッシュレス決済","交通ICカード","その他"]
-                        $0.value = "現金"
-                        
-                        UserDefaults.standard.string(forKey: "Register")
-                }
-                
-                form +++ Section()
-                    <<< IntRow("amountofmoney"){
-                        $0.title = "金額"
-                        
-                        UserDefaults.standard.string(forKey: "Value")
-                }
-                
-                form +++ Section()
-                    <<< PushRow<String>("incomeclassification"){
-                        $0.title = "分類"
-                        $0.options = ["給料","おこづかい","臨時収入","副業","その他"]
-                        $0.value = "給料"
-                        
-                        UserDefaults.standard.string(forKey: "IncomeGroup")
-                }
-                
-                form +++ Section()
-                <<< SwitchRow("switchbutton"){
-                    $0.title = "現金残高から引く"
-                }
-                
-            }
-            
-            UserDefaults.standard.bool(forKey: "isIncome")
+           
 
+            UserDefaults.standard.bool(forKey: "isIncome")
+            
         }
-    
+        
+            
+        if incomejudge == "支出" {
+            form +++ Section()
+                <<< DateInlineRow("date"){
+                    $0.title = "日付"
+                    //$0.value = "Date"
+                    
+                    UserDefaults.standard.data(forKey: "Date")
+            }
+                
+            form +++ Section()
+                <<< PushRow<String>("method"){
+                    $0.title = "支払い方法"
+                    $0.options = ["現金","キャッシュレス決済","交通ICカード","その他"]
+                    $0.value = "現金"
+                      
+                    UserDefaults.standard.string(forKey: "PaymentMethod")
+                }
+                
+            form +++ Section()
+                <<< IntRow("amountofmoney"){
+                    $0.title = "金額"
+                    
+                    UserDefaults.standard.string(forKey: "Value")
+                }
+                
+            form +++ Section()
+                <<< PushRow<String>("classification"){
+                    $0.title = "分類"
+                    $0.options = ["食費","交通費","交際費","娯楽費","被服費","その他"]
+                    $0.value = "食費"
+                        
+                    UserDefaults.standard.string(forKey: "Group")
+                }
+        } else if incomejudge == "収入" {
+            form +++ Section()
+                <<< DateInlineRow("date"){
+                    $0.title = "日付"
+                    //$0.value = "Date"
+                       
+                    UserDefaults.standard.data(forKey: "Date")
+            }
+                
+            form +++ Section()
+                <<< PushRow<String>("register"){
+                    $0.title = "登録先"
+                    $0.options = ["現金","キャッシュレス決済","交通ICカード","その他"]
+                    $0.value = "現金"
+                        
+                    UserDefaults.standard.string(forKey: "Register")
+            }
+                
+            form +++ Section()
+                <<< IntRow("amountofmoney"){
+                    $0.title = "金額"
+                        
+                    UserDefaults.standard.string(forKey: "Value")
+            }
+                
+            form +++ Section()
+                <<< PushRow<String>("incomeclassification"){
+                    $0.title = "分類"
+                    $0.options = ["給料","おこづかい","臨時収入","副業","その他"]
+                    $0.value = "給料"
+                
+                    UserDefaults.standard.string(forKey: "IncomeGroup")
+            }
+                
+            form +++ Section()
+                 <<< SwitchRow("switchbutton"){
+                    $0.title = "現金残高から引く"
+            }
+                
+        }
+            
     }
     
     
